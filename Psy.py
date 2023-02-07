f8cd34bbc383ada6e0b7a6a6183a574f5136619b
romanskrypka <58446922+romanskrypka@users.noreply.github.com>
2023-02-07 07:58:41 +0300

    PY_1

---
+++ Psy.py
@@ -0,0 +1,52 @@
+hello = "Hello World"
+#print(hello)
+x = 1
+y = 2
+z = x + y
+#print(z)
+#print(2+2)
+#print(15-2)
+#print(3*7)
+#print(7/2)
+#print(2**8)
+#print(7%2)
+
+s1 = "Hello1"
+s2 = 'Hello2'
+s3 = """Hello3"""
+
+#print(s1)
+#print(s2)
+#print(s3)
+
+greeting = "Hello " + "World"
+#print(greeting)
+l = len(greeting)
+#print(l)
+#print(len(s1))
+
+strings = ["Roman", "Nastya"]
+numbers = [1, 2, 3, 4, 5]
+data = ["Hi", 9]
+
+#print(strings)
+#print(numbers)
+#print(data)
+
+summa = numbers + data
+#print(summa)
+
+numbers.append(6)
+#print(numbers)
+
+first = strings[0]
+print(first)
+second = strings[1]
+print(second)
+#print(strings[2])
+
+strings_length = len(strings)
+print(strings_length)
+
+s = sum(numbers)
+print(s)
---
+++ dictionary.py
@@ -0,0 +1,11 @@
+# Ключ : Значение
+dictionary = {
+   "Cat": "Кошка",
+   "Bat": "Летучая мышь"
+}
+
+print(dictionary)
+
+cat = dictionary["Cat"]
+print(cat)
+
