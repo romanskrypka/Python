9312089ec71137344164accb04ef79a4a5ba0de4
romanskrypka <58446922+romanskrypka@users.noreply.github.com>
2023-02-08 07:52:11 +0300

    PY2

--- dictionary.py
+++ dictionary.py
@@ -1,11 +1,39 @@
 # Ключ : Значение
-dictionary = {
-   "Cat": "Кошка",
-   "Bat": "Летучая мышь"
+# dictionary = {
+#    "Cat": "Кошка",
+#    "Bat": "Летучая мышь"
+# }
+# контрл + / позволяет закоментировать сразу несколько строк
+#print(dictionary)
+
+#cat = dictionary["Cat"]
+#print(cat)
+
+countries = {
+   "Африка": ["Египет", "Конго", "ЮАР"],
+   "Азия": ["Китай", "Таиланд", "Индонезия"]
 }
 
-print(dictionary)
+africa = countries["Африка"]
+#print(africa)
 
-cat = dictionary["Cat"]
-print(cat)
+africa_key = "Африка"
+#print(countries[africa_key])
+
+countries["Европа"] = ["Австрия", "Испания", "Италия"]
+#print(countries)
+#print(countries["Европа"])
+
+# countries[0] = "Hello"
+# print(countries)
+
+# africa.append("Эфиопия")
+# print(africa)
+# print(countries)
+# print(len(countries["Африка"]))
+
+name = input("Введите имя: ")
+#print(name)
+
+print("Привет", name)
 
